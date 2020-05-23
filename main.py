cf777089cbea97c6b470d550deeb2b39cdbe1d00
daiki <daiki.nitta888@gmail.com>
2020-05-23 14:22:08 +0900

    どっちのPCでも起動するように調整

--- main.py
+++ main.py
@@ -10,16 +10,30 @@ import pathlib
 import pyocr
 from PIL import Image
 
-# pdfsファイル作ってね
-pdf_files = pathlib.Path('pdfs').glob('*.pdf')
 
-img_dir = pathlib.Path('out_img')
-pdf_files = pathlib.Path('pdfs').glob('*.pdf')
 
+pdfs = pathlib.PurePath.joinpath(pathlib.Path.cwd(), 'pdfs')
+if not pdfs.exists():
+    print('pdfsファイルを作成してください')
+pdf_files = pdfs.glob('*.pdf')
+
+img_dir = pathlib.PurePath.joinpath(pathlib.Path.cwd(), 'out_img')
+if not img_dir.exists():
+    img_dir.mkdir()
+
+
+poppler = pathlib.Path(r'C:\Users\g2945\poppler\bin')
+if not poppler.exists():
+    poppler = pathlib.Path(r'C:\poppler\bin')
+
+if not poppler in os.environ["PATH"].split(os.pathsep):
+    os.environ["PATH"] += os.pathsep + str(poppler)
+
+# pdf→jpg
 for pdf_file in pdf_files:
     imgs2 = pdf2image.convert_from_path(pdf_file)
     pgs = str(len(imgs2))
-    title = pdf_file.stem + '_ '+ pgs + 'p'
+    title = pdf_file.stem + '_' + pgs + 'p'
     title_dir = pathlib.PurePath.joinpath(img_dir, title)
     if not title_dir.exists():
         title_dir.mkdir()
@@ -29,16 +43,33 @@ for pdf_file in pdf_files:
     else:
         print('<', pdf_file.stem, '>はすでに変換済みです', sep='')
 
-# JPEG → excell
-tesseract_path = (r'C:\Users\g2945\tesseract')
 
-if tesseract_path not in os.environ["PATH"].split(os.pathsep):
-    os.environ["PATH"] += os.pathsep + tesseract_path
+# JPEG → excell
+
+# 環境変数にテッセラクトのパス通す
+tesseract_path = pathlib.Path(r'C:\Users\g2945\tesseract')
+if not tesseract_path.exists():
+    tesseract_path = pathlib.Path(r'C:\tesseract')
+
+    if tesseract_path not in os.environ["PATH"].split(os.pathsep):
+        os.environ["PATH"] += os.pathsep + str(tesseract_path)
 
 tools = pyocr.get_available_tools()
 tool = tools[0]
-
-img = Image.open(r'C:\Users\g2945\Desktop\jupyter\OCR\out_img\入札調書（R1-2窪川）_ 2p\pg-1.jpg')
-
 builder = pyocr.builders.TextBuilder()
-result = tool.image_to_string(img, lang="jpn", builder=builder)
+
+# 画像データ取得
+imgs = []
+img_d_dir = img_dir.glob('*')
+for a in img_d_dir:
+    img_list = a.glob('*.jpg')
+    img = Image.open(list(img_list)[0])
+    imgs.append(img)
+
+# 画像データに対してocr実行
+results = []
+for index, img in enumerate(imgs):
+        result = tool.image_to_string(img, lang="jpn", builder=builder)
+        results.append(result)
+
+
